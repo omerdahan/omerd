b5b0606c0cbdb8940b3450e93aeb9adc266e79b5
omerdahan <omerd@localhost.localdomain>
2018-09-09 23:15:35 +0300

    ex2 answer

---
+++ ex2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  ex2.py
+#  
+#  Copyright 2018 Omer Dan <omerd@localhost.localdomain>
+
+###declare an array of 3 strings from the user
+
+string1 = input('please enter string1: ')
+string2 = input('please enter string2: ')
+string3 = input('please enter string3: ')
+
+string4 = input('please enter string4: ')
+string5 = input('please enter string5: ')
+
+string1=string4
+string3=string5
+
+string6 = input('please enter 3 numbers: ')
+string2=string6
+
+print(string6[1])
