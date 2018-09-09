43f5b44a827ee407a86e7d92f84eea949ba74f76
omerdahan <omerd@localhost.localdomain>
2018-09-09 23:15:48 +0300

    ex3 answer

---
+++ ex3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  ex2.py
+#  
+#  Copyright 2018 Omer Dan <omerd@localhost.localdomain>
+
+###declare an array of 3 strings from the user
+
+string1 = input('please enter string1: ')
+string2 = input('please enter string2: ')
+string3 = input('please enter string3: ')
+##replace the first tne third item in the array
+string4 = input('please enter string4: ')
+string5 = input('please enter string5: ')
+
+string1=string4
+string3=string5
+##replace the second item with other array of 3 numbers
+string6 = input('please enter 3 numbers: ')
+string2=string6
+##print the second number in the inner array
+print(string6[1])
